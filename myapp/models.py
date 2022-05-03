17ad4a615809a07bcef221c1da7fd443ddd6ef17
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-05-03 07:36:45 +0200

    adding tipo_establecimiento to establecimientos model

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-
+import string, random
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -17,7 +20,12 @@ class Servicios(models.Model):
         return self.nombre
 
 
-
+TIPO_ESTABLECIMIENTO = (
+   ('Restaurante', 'Restaurante'),
+   ('Cafeteria', 'Cafeteria'),
+   ('Bar', 'Bar'),
+   ('CafeBar', 'CafeBar')
+)
 class Establecimientos(models.Model):
     nombre = models.CharField(max_length=50, primary_key=True)
     categoria = models.ForeignKey(Categorias, on_delete=models.PROTECT)
@@ -30,11 +38,10 @@ class Establecimientos(models.Model):
     tarjeta_credito = models.BooleanField()
     tarjetas_debito = models.BooleanField()
     descripcion = models.TextField()
-    restaurante = models.BooleanField()
-    cafeteria = models.BooleanField()
-    bar = models.BooleanField()
+    tipo = models.CharField(max_length=10, choices=TIPO_ESTABLECIMIENTO, default='Restaurante')
     direccion = models.CharField(max_length=50)
     imagen = models.ImageField(upload_to='productos', null=True)
+    slug = models.SlugField(max_length=250, null=True, blank=True)
 
     def __str__(self):
         return self.nombre
