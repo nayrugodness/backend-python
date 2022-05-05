242f746c7888cef70104d234a8dfdf61936734a9
Nayru <nanualexandraramirez@gmail.com>
2022-05-04 22:16:56 -0500

    deleting useless models

--- myapp/models.py
+++ myapp/models.py
@@ -5,27 +5,6 @@ from django.dispatch import receiver
 from django.utils.text import slugify
 # Create your models here.
 
-def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
-
-
-def unique_slug_generator(instance, new_slug=None):
-    if new_slug is not None:
-        slug = new_slug
-    else:
-        slug = slugify(instance.title)
-    Klass = instance.__class__
-    max_length = Klass._meta.get_field('slug').max_length
-    slug = slug[:max_length]
-    qs_exists = Klass.objects.filter(slug=slug).exists()
-
-    if qs_exists:
-        new_slug = "{slug}-{randstr}".format(
-            slug=slug[:max_length - 5], randstr=random_string_generator(size=4))
-
-        return unique_slug_generator(instance, new_slug=new_slug)
-    return slug
-
 
 class Categorias(models.Model):
     nombre = models.CharField(max_length=50, primary_key=True)
@@ -47,6 +26,7 @@ TIPO_ESTABLECIMIENTO = (
    ('Bar', 'Bar'),
    ('CafeBar', 'CafeBar')
 )
+
 class Establecimientos(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
@@ -62,11 +42,14 @@ class Establecimientos(models.Model):
     descripcion = models.TextField()
     tipo = models.CharField(max_length=20, choices=TIPO_ESTABLECIMIENTO, default='Restaurante')
     direccion = models.CharField(max_length=50)
-    imagen = models.ImageField(upload_to='productos', null=True)
-    slug = models.SlugField(max_length=250, null=True, blank=True)
+    imagen = models.ImageField(upload_to='productos', null=False)
+    imagen_banner = models.ImageField(upload_to='productos', null=False)
+    slug = models.SlugField(max_length=250, null=False, blank=True)
 
     def __str__(self):
         return self.nombre
+
+
 
 @receiver(pre_save, sender=Establecimientos)
 def pre_save_receiver(sender, instance, *args, **kwargs):
