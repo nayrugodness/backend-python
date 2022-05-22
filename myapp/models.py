0435e28d474a6eaafe37a330a4e0bc409af11e6b
Nayru <nanualexandraramirez@gmail.com>
2022-05-22 15:51:34 -0500

    adding precio field migration

--- myapp/models.py
+++ myapp/models.py
@@ -6,7 +6,7 @@ class ItemMenu(models.Model):
     id = models.AutoField(primary_key=True)
     descripcion = models.TextField()
     foto = models.ImageField(upload_to='establecimientos/platillo', null=False)
-    precio = models.CharField(max_length=10)
+    precio = models.CharField(max_length=10, default=0)
 
 Categoria=(
     ('Buffet', 'Buffet'),
